270378fd66460e751ad490a3808ca3eaeddd050c
elfpower <elfpower@qq.com>
2018-03-30 18:05:08 +0800

    edit search get data
    编辑搜索数据

---
+++ src/api/searchData.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export function searchProduct (oParams) {
+  const url = '/api/searchProduct'
+  const commonParams = {}
+  const searchParams = oParams || {}
+  const data = Object.assign({}, commonParams, {
+    keyword: searchParams.keyword || '',
+    categoryID: searchParams.categoryID || '',
+    price_min: searchParams.price_min || '',
+    price_max: searchParams.price_max || '',
+    brandID: searchParams.brandID || '',
+    sort: searchParams.sort || 1,
+    pageIndex: searchParams.pageIndex || 1,
+    pageSize: searchParams.pageSize || 20
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
