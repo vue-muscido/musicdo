db5dfe089dd5b3fd9e5f2e3ef7fc78fdf56e472a
elfpower <elfpower@qq.com>
2018-04-12 17:42:48 +0800

    搜索列表页监听keyword渲染列表实现
    搜索列表页监听keyword渲染列表实现

--- src/common/js/localStore.js
+++ src/common/js/localStore.js
@@ -1,6 +1,5 @@
 export function localSave (key, val) {
   localStorage.setItem(key, JSON.stringify(val))
-  console.log(JSON.stringify(val))
 }
 export function localTake (key) {
   return JSON.parse(localStorage.getItem(key))
