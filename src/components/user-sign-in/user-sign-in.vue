d83b75989d71ee6f02a98f72dbeb2987376f15e0
elfpower <elfpower@qq.com>
2018-04-21 18:01:55 +0800

    添加登陆页面基本结构
    添加登陆页面基本结构

---
+++ src/components/user-sign-in/user-sign-in.vue
@@ -0,0 +1,135 @@
+<template >
+  <div class="user-sign-in" >
+    <div class="logo-bar" >
+      <div class="logo" ></div >
+    </div >
+    
+    <div class="input-bar">
+      <cube-input
+        class="input-account"
+        v-model="userAccount.value"
+        :placeholder="userAccount.placeholder"
+        :type="userAccount.type"
+        :maxlength="userAccount.maxlength"
+        :readonly="userAccount.readonly"
+        :disabled="userAccount.disabled"
+        :autofocus="userAccount.autofocus"
+        :autocomplete="userAccount.autocomplete"
+        :clearable="userAccount.clearable"
+        @input="accountInput()"
+      ></cube-input >
+      
+      <cube-input
+        class="input-passward"
+        v-model="userPassword.value"
+        :placeholder="userPassword.placeholder"
+        :type="userPassword.type"
+        :maxlength="userPassword.maxlength"
+        :readonly="userPassword.readonly"
+        :disabled="userPassword.disabled"
+        :autofocus="userPassword.autofocus"
+        :autocomplete="userPassword.autocomplete"
+        :clearable="userPassword.clearable"
+        :eye="userPassword.eye"
+      ></cube-input >
+    </div>
+    
+    <div class="quick-bar">
+      <div>
+        短信快速登录
+      </div>
+      <div>
+        忘记密码
+      </div>
+    </div>
+    
+    <div class="btn-bar">
+      登录
+    </div>
+    
+    <div class="new-account-bar">
+      注册新账号
+    </div>
+    
+    <div class="other-way-bar">
+      其他登陆
+    </div>
+    
+  </div >
+</template >
+
+<script type="text/ecmascript-6" >
+export default {
+  // 别名
+  name: 'user-sign-in',
+  // 数据
+  data () {
+    return {
+      userAccount: {
+        value: '账号账号账号账号账号账号',
+        placeholder: '手机/用户名/邮箱',
+        type: 'text',
+        readonly: false,
+        maxlength: 16,
+        disabled: false,
+        autofocus: true,
+        autocomplete: true,
+        clearable: true
+      },
+      userPassword: {
+        value: '',
+        placeholder: '请输入密码',
+        type: 'password',
+        readonly: false,
+        maxlength: 16,
+        disabled: true,
+        autofocus: false,
+        autocomplete: false,
+        clearable: false,
+        eye: true
+      }
+    }
+  },
+  // 子组件
+  components: {},
+  // 接受父组件传递数据
+  props: {},
+  // 组件实例创建前
+  beforeCreate () {},
+  // 组件实例创建后
+  created () {},
+  // 模板编译/挂载前
+  beforeMount () {},
+  // 模板编译/挂载后
+  mounted () {},
+  // 组件更新前
+  beforeUpdate () {},
+  // 组件更新后
+  updated () {},
+  // 组件销毁前
+  beforeDestroy () {},
+  // 组件销毁后
+  destroyed () {},
+  // 方法集合
+  methods: {
+    accountInput () {
+      if (this.userAccount.value !== '') {
+        console.log(this.userPassword.disabled, this.userAccount.value)
+        this.userPassword.disabled = false
+      } else {
+        console.log(this.userPassword.disabled, this.userAccount.value)
+        this.userPassword.disabled = true
+      }
+    }
+  },
+  // 实时计算数据（一个数据受多个数据影响）
+  computed: {},
+  // 实时计算数据（一个数据影响多个数据）
+  watch: {}
+}
+</script >
+
+<style scoped lang="stylus" rel="stylesheet/stylus" >
+.user-sign-in
+  background red
+</style >
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Sort from 'components/sort/sort' // 分类页组件
 import Cart from 'components/cart/cart' // 购物车组件
 import User from 'components/user/user' // 用户中心组件
 
+import UserSignIn from 'components/user-sign-in/user-sign-in' // 用户登录组件
+
 import MainSearch from 'components/main-search/main-search' // 搜索页
 import SearchList from 'components/search-list/search-list' // 搜索列表Ω
 
@@ -39,6 +41,10 @@ export default new Router({
     {
       path: '/user',
       component: User
+    },
+    {
+      path: '/user-sign-in',
+      component: UserSignIn
     },
     {
       path: '/main-search',
