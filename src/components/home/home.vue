ba0948abe5fd49328e40d387512f60c5095464d6
titan_oO <elfpower@qq.com>
2018-04-25 21:48:07 +0800

    eslint修改
    eslint修改

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -105,7 +105,7 @@ export default {
           nteractive: false // 1.8.0 新增
         }
       },
-      tabList:[0,1,2,3,4,5,6,7,8,9]
+      tabList: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     }
   },
   created () {
