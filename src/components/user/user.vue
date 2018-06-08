d1f65a429e3c1db09135a1e0329abb77d9c9b250
ilssn <ilssn@icloud.com>
2018-06-08 14:03:46 +0800

    用户中心界面完成

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -8,50 +8,50 @@
       </div>
       <div class="bar-show">
         <div class="photo">
-          <img src="" alt="">
+          <img :src="userMsg.ImageUrl" alt="">
         </div>
         <div class="text">
-          <h2 class="user-name">JINGJING028</h2>
+          <h2 class="user-name">{{userMsg.NickName}}</h2>
           <p class="user-lever">普通用户</p>
         </div>
       </div>
     </div>
-    <!-- 我的订单 -->
-    <div class="user-order">
-      <div class="bar-order">
-        <div class="my-order">我的订单</div>
-        <div class="btn-show-all">全部订单</div>
-      </div>
-      <div class="order-type">
-        <div class="btn-order">
-          <div class="icon"></div>
-          <div class="text">待付款</div>
+    <!-- 滚动开始 -->
+    <div class="scroll" >
+      <cube-scroll
+        ref="scroll"
+        :data="privateList"
+        :options="options">
+        <!-- 我的订单 -->
+        <div class="user-order">
+          <div class="bar-order">
+            <div class="my-order">我的订单</div>
+            <div class="btn-show-all">全部订单</div>
+          </div>
+          <ul class="order-type">
+            <li v-for="(tab, index) in tabList" :key="index" @click="tabClick(tab.name, index)" class="btn-order">
+              <div class="icon" :class="tab.pic"></div>
+              <div class="text">{{tab.name}}</div>
+            </li>
+          </ul>
         </div>
-        <div class="btn-order">
-          <div class="icon"></div>
-          <div class="text">待发货</div>
-        </div>
-        <div class="btn-order">
-          <div class="icon"></div>
-          <div class="text">待收货</div>
-        </div>
-        <div class="btn-order">
-          <div class="icon"></div>
-          <div class="text">待评价</div>
-        </div>
-        <div class="btn-order">
-          <div class="icon"></div>
-          <div class="text">退换货</div>
-        </div>
-      </div>
-    </div>
-    <!-- 用户关于 -->
-    <div class="user-about">
-      <div class="btn-about">
-        <div class="icon">#</div>
-        <div class="text">宝贝收藏</div>
-      </div>
-    </div>
+        <!-- 用户关于 -->
+        <ul class="user-about">
+          <li v-for="(about, index) in aboutList" :key="index" @click="aboutClick(about.name, index)" class="btn-about">
+            <div class="icon" :class="about.pic"></div>
+            <div class="text">{{about.name}}</div>
+          </li>
+        </ul>
+        <!-- 用户私人信息 -->
+        <ul class="user-about user-private">
+          <li v-for="(about, index) in privateList" :key="index" @click="aboutClick(about.name, index)" class="btn-about">
+            <div class="icon" :class="about.pic"></div>
+            <div class="text">{{about.name}}</div>
+          </li>
+        </ul>
+      </cube-scroll >
+    </div >
+    <!-- 滚动结束 -->
     <div
       style="display: none;position: fixed;top: 0;z-index: 99999;width: 100%;max-width: 750px;max-height: 50vh;background-color: rgba(0,0,0,.5);color: #fff" >
       <div style="background-color: red;color: #fff;text-align: center" @click="outSign()" >点击退出登录</div >
@@ -64,12 +64,22 @@
 
 <script type="text/ecmascript-6" >
 import Tab from 'components/tab/tab'
+import { tabData, aboutData, privateData } from 'api/userData'
 // import { localTake } from 'common/js/localStore'
 import { mapGetters, mapActions } from 'vuex'
 export default {
   data () {
     return {
-      local: 'http://musicdo.cn'
+      local: 'http://musicdo.cn',
+      tabList: [],
+      aboutList: [],
+      PrivateList: [],
+      options: {
+        scrollbar: {
+          fade: true,
+          nteractive: false // 1.8.0 新增
+        }
+      }
     }
   },
   computed: {
@@ -80,14 +90,29 @@ export default {
       return JSON.stringify(this.userMsg)
     }
   },
+  created () {
+    this._getTabData()
+  },
   components: {
     Tab
   },
   mounted () {
   },
-  created () {
-  },
   methods: {
+    // 获取订单状态tab列表
+    _getTabData () {
+      this.tabList = tabData
+      this._getAboutData()
+    },
+    // 获取用于关于列表
+    _getAboutData () {
+      this.aboutList = aboutData
+      this._getPrivateData()
+    },
+    // 获取用户私人信息
+    _getPrivateData () {
+      this.privateList = privateData
+    },
     // 退出登陆
     outSign () {
       this.xRrmoveUserMsg('')
